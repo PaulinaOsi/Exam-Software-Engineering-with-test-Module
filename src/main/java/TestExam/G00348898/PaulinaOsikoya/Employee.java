3e1716e1cd76eed5cfc1964bcb20681fcedc5bb0
PaulinaOsikoya99 <paulinaosikoya@gmail.com>
2020-03-06 12:35:33 +0000

    adding to structure

---
+++ src/main/java/TestExam/G00348898/PaulinaOsikoya/Employee.java
@@ -0,0 +1,11 @@
+/*
+ Name:Paulina Osikoya
+ G00348898
+ lecturer:Paul lennon
+
+ Brief : creat an Employee class with the relevant information in order to carry out the tests in the Employeee test class
+
+ */
+package TestExam.G00348898.PaulinaOsikoya;
+public class Employee {
+}
---
+++ src/test/java/TestExam/G00348898/PaulinaOsikoya/EmployeeTest.java
@@ -0,0 +1,24 @@
+/*
+ Name:Paulina Osikoya
+ G00348898
+ lecturer:Paul lennon
+
+ Brief : creat an Employee class with the relevant information in order to carry out the tests in the Employeee test class
+
+ */
+package TestExam.G00348898.PaulinaOsikoya;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+
+public class EmployeeTest {
+
+    @BeforeAll
+    @DisplayName("Starting the Test")
+    public static void print(){
+        System.out.println("Starting the tests.");
+    }
+
+}
