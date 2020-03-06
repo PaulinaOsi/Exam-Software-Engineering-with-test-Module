1aebf9b1b6cf33ae8274e725317724f25886af36
PaulinaOsikoya99 <paulinaosikoya@gmail.com>
2020-03-06 13:42:02 +0000

    adding to structure v2

--- src/main/java/TestExam/G00348898/PaulinaOsikoya/Employee.java
+++ src/main/java/TestExam/G00348898/PaulinaOsikoya/Employee.java
@@ -8,4 +8,137 @@
  */
 package TestExam.G00348898.PaulinaOsikoya;
 public class Employee {
+
+/*****************VARS*********************/
+private String title; // mr ms mrs
+private String name; // min 5 max 22
+private String pps; // 6 char length
+private int phone; //7 digits
+private String employmentType; //full-time / part-time
+private int age; //over 18
+
+    /****************CONSTRUCTOR*********************/
+    public Employee(String title, String name, String pps, int phone, String employmentType, int age) {
+
+        //error checking
+        //title
+        if(title.equalsIgnoreCase("Mr")||title.equalsIgnoreCase("Mrs")||title.equalsIgnoreCase("Ms")){
+            this.title = title;
+        }else{
+            throw  new IllegalArgumentException("invalid title");
+        }
+
+        //name
+        if(name.length()>5 || name.length()<=22){
+            this.name = name;
+        }else{
+
+            throw  new IllegalArgumentException("invalid name");
+        }
+
+        //pps
+        if(pps.length()<6){
+            throw  new IllegalArgumentException("invalid pps");
+        }else{
+            this.pps = pps;
+        }
+
+        //phone
+        if(phone>=9999999){
+            throw  new IllegalArgumentException("invalid number");
+        }else{
+            this.phone = phone;
+        }
+
+        //emptype
+        if(employmentType.equalsIgnoreCase("part-time")||employmentType.equalsIgnoreCase("full-time")){
+            this.employmentType = employmentType;
+        }else{
+            throw  new IllegalArgumentException("invalid employment type");
+        }
+
+        //age
+        if(age<18){
+            throw  new IllegalArgumentException("invalid age - too young");
+        }else{
+            this.age = age;
+        }
+
+    }
+
+    /****************GETTERS AND SETTERS********************/
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        if(title.equalsIgnoreCase("Mr")||title.equalsIgnoreCase("Mrs")||title.equalsIgnoreCase("Ms")){
+            this.title = title;
+        }else{
+            throw  new IllegalArgumentException("invalid title");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if(name.length()>5 || name.length()<=22){
+            this.name = name;
+
+        }else{
+            throw  new IllegalArgumentException("invalid name");
+        }
+    }
+
+    public String getPps() {
+        return pps;
+    }
+
+    public void setPps(String pps) {
+        if(pps.length()<6){
+            throw  new IllegalArgumentException("invalid pps");
+        }else{
+            this.pps = pps;
+        }
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        if(phone>=1000000){
+            throw  new IllegalArgumentException("invalid number");
+        }else{
+            this.phone = phone;
+        }
+    }
+
+    public String getEmploymentType() {
+        return employmentType;
+    }
+
+    public void setEmploymentType(String employmentType) {
+        if(employmentType.equalsIgnoreCase("part-time")||employmentType.equalsIgnoreCase("full-time")){
+            this.employmentType = employmentType;
+        }else{
+            throw  new IllegalArgumentException("invalid employment type");
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+        if(age<18){
+            throw  new IllegalArgumentException("invalid age - too young");
+        }else{
+            this.age = age;
+        }
+    }
 }
+
--- src/test/java/TestExam/G00348898/PaulinaOsikoya/EmployeeTest.java
+++ src/test/java/TestExam/G00348898/PaulinaOsikoya/EmployeeTest.java
@@ -14,11 +14,159 @@ import org.junit.jupiter.api.*;
 
 
 public class EmployeeTest {
+    private Employee myEmployee;
 
+   /**************************PRE TEST START**************************/
     @BeforeAll
     @DisplayName("Starting the Test")
     public static void print(){
         System.out.println("Starting the tests.");
     }
+    /*****************************************************************/
+
+
+    /**************************CREATE INSTANCE**************************/
+    @BeforeEach
+    @DisplayName("New instance of the Employee")
+    void init(TestInfo testInfo, TestReporter testReporter)
+    {
+        myEmployee = new Employee("Ms","Paulina","123456",8712345,"full-time",21);
+        System.out.println("TimeStamp = "+ testInfo.getDisplayName());
+    }
+    /*****************************************************************/
+
+    /**************************TESTS**************************/
+
+    @Test
+    @DisplayName("Testing Getter and Setters")
+    void testGettersAndSetter(TestInfo testInfo, TestReporter testReporter)
+    {
+
+        //title
+        //Mr
+        myEmployee.setTitle("Mr");
+        assertEquals(myEmployee.getTitle(),"Mr");
+        //Mrs
+        myEmployee.setTitle("Mrs");
+        assertEquals(myEmployee.getTitle(),"Mrs");
+        //Ms
+        myEmployee.setTitle("Ms");
+        assertEquals(myEmployee.getTitle(),"Ms");
+        //name
+        myEmployee.setName("john");
+        assertEquals(myEmployee.getName(),"john");
+        //pps
+        myEmployee.setPps("123456");
+        assertEquals(myEmployee.getPps(),"123456");
+        //phone
+        myEmployee.setPhone(999999);
+        assertEquals(myEmployee.getPhone(),999999);
+        //employmentType
+        //FullTime
+        myEmployee.setEmploymentType("full-time");
+        assertEquals(myEmployee.getEmploymentType(),"full-time");
+        //PartTime
+        myEmployee.setEmploymentType("part-time");
+        assertEquals(myEmployee.getEmploymentType(),"part-time");
+        //age
+        myEmployee.setAge(21);
+        assertEquals(myEmployee.getAge(),21);
+    }
+
+    @Test
+    @DisplayName("Invalid Title")
+    void testTileInvalid(TestInfo testInfo, TestReporter testReporter)
+    {
+        //title
+        //Mr
+        Assertions.assertThrows(IllegalArgumentException.class, ()->{
+            myEmployee.setTitle("Yo");
+        });
+    }
+
+    @Test
+    @DisplayName("Invalid pps")
+    void testPPSInvalid(TestInfo testInfo, TestReporter testReporter)
+    {
+        //title
+        //Mr
+        myEmployee.setPps("123456");
+        assertEquals(myEmployee.getPps(),"123456");
+        Assertions.assertThrows(IllegalArgumentException.class, ()->{
+            myEmployee.setTitle("1234");
+        });
+    }
+
+
+    @Test
+    @DisplayName("Invalid EmpType")
+    void testEmpTypeInvalid(TestInfo testInfo, TestReporter testReporter)
+    {
+        //title
+        //Mr
+        Assertions.assertThrows(IllegalArgumentException.class, ()->{
+            myEmployee.setEmploymentType("Yo");
+        });
+    }
+
+    @Test
+    @DisplayName("Invalid age")
+    void testAgeInvalid(TestInfo testInfo, TestReporter testReporter)
+    {
+        //age
+        Assertions.assertThrows(IllegalArgumentException.class, ()->{
+            myEmployee.setAge(16);
+        });
+    }
+
+
+    @Test
+    @DisplayName("Invalid phone")
+    void testPHONEInvalid(TestInfo testInfo, TestReporter testReporter)
+    {
+        //Phone
+        Assertions.assertThrows(IllegalArgumentException.class, ()->{
+            myEmployee.setPhone(999999999);
+        });
+    }
+
+    @Test
+    @DisplayName("valid Constructor")
+    void testConstructorvalid(TestInfo testInfo, TestReporter testReporter)
+    {
+
+        myEmployee = new Employee("Ms","Paulina","123456",8712345,"full-time",21);
+
+        assertEquals(myEmployee.getTitle(),"Ms");
+
+        assertEquals(myEmployee.getName(),"Paulina");
+
+        assertEquals(myEmployee.getPps(),"123456");
+
+        assertEquals(myEmployee.getPhone(),8712345);
+
+        assertEquals(myEmployee.getEmploymentType(),"full-time");
+
+        assertEquals(myEmployee.getAge(),21);
+    }
+
+    @Test
+    @DisplayName("invalidvalid Constructor")
+    void testConstructorinvalid(TestInfo testInfo, TestReporter testReporter)
+    {
+
+        Assertions.assertThrows(IllegalArgumentException.class, ()->{
+            new Employee("Ms","Paulina","123456",8712345,"full-time",16);
+        });
+
+    }
+
+    /**************************POST TEST DONE**************************/
+    @AfterAll
+    @DisplayName("Ending the Test")
+    public static void done(){
+        System.out.println("Completed the tests.");
+    }
+    /*****************************************************************/
 
 }
